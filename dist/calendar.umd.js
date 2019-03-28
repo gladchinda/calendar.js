8a203350babaf01fe22098d8c81baa534502d2f8
Glad Chinda <gladxeqs@gmail.com>
2019-03-28 17:29:42 +0100

    Added initial src files and dist build

---
+++ dist/calendar.umd.js
@@ -0,0 +1,2 @@
+!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?module.exports=t():"function"==typeof define&&define.amd?define(t):e.calendarJs=t()}(this,function(){var e=function(){var e=new Date;this.today=e,this.month=e.getMonth(),this.year=e.getFullYear()};return function(){return new e}});
+//# sourceMappingURL=calendar.umd.js.map
